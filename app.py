5f434a44c5d74901f9623adfec7d94eaf4d7ceb2
mmgili <82985206+mmgili@users.noreply.github.com>
2022-04-11 12:33:55 -0300

    Agregar opción descargar CSV

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import pandas as pd
 import spacy
 ##import csv
 
+@st.cache
+def convert_df(df):
+   return df.to_csv(sep=";", index=False, quoting=3).encode('utf-8') #3 = csv.QUOTE_NONE
+
 def lemas(text, nlp):
     text = nlp(text)
     lista = [token.lemma_.lower() for token in text if token.pos_ in [u'NOUN', u'ADJ', u'PROPN', u'VERB']]
     return ','.join(lista)
 
 st.title('Lematiza CSV')
+st.text('Cargar archivo CSV separado con punto y coma.')
+st.text('Primera Columna: ID')
+st.text('Segunda Columna: Texto')
 
 data = st.file_uploader("Cargar CSV", type=["csv"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
@@ -19,3 +26,10 @@ if data is not None:
     dataframe = pd.read_csv(data, delimiter=";")
     dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(lemas, args=(nlp,))
     st.write(dataframe)
+    st.download_button(
+       "Descargar salida",
+       convert_df(dataframe),
+       "Salida.csv",
+       "text/csv",
+       key='download-csv'
+    )
