1f814ba6f95a41516dedb8d490e00c7891984e1c
mmgili <82985206+mmgili@users.noreply.github.com>
2022-04-15 10:44:41 -0300

    Agregado conteo

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def convert_df(df):
    return df.to_csv(sep=";", index=False, quoting=3).encode('utf-8') #3 = csv.QUOTE_NONE
 
 def lemas(text, nlp):
-    text = nlp(text)
-    lista = [token.lemma_.lower() for token in text if token.pos_ in [u'NOUN', u'ADJ', u'PROPN', u'VERB']]
-    return ','.join(lista)
+    text = nlp(text.lower())
+    lista = [token.lemma_ for token in text if token.pos_ in [u'NOUN', u'ADJ', u'PROPN', u'VERB']]
+    return lista #','.join(lista)
 
 st.title('Lematiza CSV')
 st.text('Cargar archivo CSV separado con punto y coma.')
@@ -25,7 +25,18 @@ if data is not None:
     nlp = spacy.load('es_core_news_sm', disable=['ner'])
     dataframe = pd.read_csv(data, delimiter=";")
     dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(lemas, args=(nlp,))
-    st.write(dataframe)
+    count = {}
+    for index, value in dataframe["Lemas"].items():
+        for palabra in value:
+            count.setdefault(palabra, 0)
+            count[palabra] = count[palabra] + 1
+
+    ncount = sorted(
+        [[k, v, i] for i, (k, v) in enumerate(count.items())], key=lambda r:r[1], reverse=True)
+
+    st.write(ncount)
+    #st.write(dataframe)
+    
     st.download_button(
        "Descargar salida",
        convert_df(dataframe),
