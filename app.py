09672ee7844db8318131ffe96231fda43450292a
mmgili <82985206+mmgili@users.noreply.github.com>
2022-04-30 10:47:14 -0300

    Acomodado interfaz
    Reordenado, agregado botón.

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ st.text('Cargar archivo CSV separado con punto y coma.')
 st.text('Primera Columna: ID')
 st.text('Segunda Columna: Texto')
 
+data = st.file_uploader("Cargar CSV", type=["csv"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+
 t_proceso = st.radio(
     "Tipo de proceso",
     ('Contar palabras normal', 'Para nube de palabras'), index=0)
      
-cant_palabras = st.number_input('Cantidad de palabras', min_value=1, value=50)
 t_nube = (t_proceso == 'Contar palabras normal')
 if t_proceso == 'Para nube de palabras':
     C_NOUN = st.checkbox('Sustantivos comunes', value=True)
@@ -50,47 +51,48 @@ if t_proceso == 'Para nube de palabras':
     if C_VERB:
         lista_pos.append(u'VERB')
 
-data = st.file_uploader("Cargar CSV", type=["csv"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+cant_palabras = st.number_input('Cantidad de palabras', min_value=1, value=50)
 
 if data is not None:
-##with open(data, newline='') as csvfile:
-    ##csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    nlp = spacy.load('es_core_news_sm', disable=['ner'])
-    dataframe = pd.read_csv(data, delimiter=";")
-    if t_proceso == 'Para nube de palabras':
-        dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(lemas, args=(nlp, lista_pos,))
-    else:
-        dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(contar)
+    if st.button("Correr!"):
+        nlp = spacy.load('es_core_news_sm', disable=['ner'])
+        dataframe = pd.read_csv(data, delimiter=";")
+        if t_proceso == 'Para nube de palabras':
+            dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(lemas, args=(nlp, lista_pos,))
+        else:
+            dataframe["Lemas"] = dataframe[dataframe.columns[1]].apply(contar)
 
-    count = {}
-    for index, value in dataframe["Lemas"].items():
-        for palabra in value:
-            count.setdefault(palabra, 0)
-            count[palabra] = count[palabra] + 1
-
-    ncount = sorted(
-        [[k, v] for k, v in count.items()], key=lambda r:r[1], reverse=True)
-
-    for palabra in ncount[:cant_palabras]:
+        count = {}
         for index, value in dataframe["Lemas"].items():
-            if palabra[0] in value:
-                dataframe.at[index, palabra[0]] = '1'
+            for palabra in value:
+                count.setdefault(palabra, 0)
+                count[palabra] = count[palabra] + 1
 
-    pd_ncount = pd.DataFrame(ncount)
-    st.dataframe(pd_ncount)
-    st.download_button(
-       "Descargar conteo",
-       convert_df(pd_ncount),
-       "Conteo.csv",
-       "text/csv",
-       key='download-csv'
-    )
+        ncount = sorted(
+            [[k, v] for k, v in count.items()], key=lambda r:r[1], reverse=True)
 
-    st.write(dataframe)
-    st.download_button(
-       "Descargar salida",
-       convert_df(dataframe.drop(['Lemas'], axis=1)),
-       "Salida.csv",
-       "text/csv",
-       key='download-csv'
-    )
+        for palabra in ncount[:cant_palabras]:
+            for index, value in dataframe["Lemas"].items():
+                if palabra[0] in value:
+                    dataframe.at[index, palabra[0]] = '1'
+
+        dataframe["Lemas"] = dataframe["Lemas"].apply(lambda x: ' '.join(x))
+
+        pd_ncount = pd.DataFrame(ncount)
+        st.dataframe(pd_ncount)
+        st.download_button(
+           "Descargar conteo",
+           convert_df(pd_ncount),
+           "Conteo.csv",
+           "text/csv",
+           key='download-csv'
+        )
+
+        st.write(dataframe)
+        st.download_button(
+           "Descargar salida",
+           convert_df(dataframe.drop(['Lemas'], axis=1)),
+           "Salida.csv",
+           "text/csv",
+           key='download-csv'
+        )
